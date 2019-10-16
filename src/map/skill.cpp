a1ef961856818fbacc59acbaed356ad82161e525
Seravy <SeravySensei@gmail.com>
2019-10-16 19:18:12 +0200

    SeraRO

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -17250,12 +17250,9 @@ int skill_autospell(struct map_session_data *sd, uint16 skill_id)
 #ifdef RENEWAL
 		 if (sd->sc.data[SC_SPIRIT] && sd->sc.data[SC_SPIRIT]->val2 == SL_SAGE && skill_lv < 4)
 		 maxlv = 10; //Soul Linker bonus. [Skotlex]
-	else {
-		maxlv = skill_id / 2; // Half of Autospell's level unless player learned a higher level
-		if (sd && pc_checkskill(sd, skill_id) > maxlv)
-		 maxlv = pc_checkskill(sd, skill_id);
-	
-	}
+	else 
+			 maxlv = skill_lv / 2; // Half of Autospell's level unless player learned a lower level
+
 #else
 
 
@@ -17279,8 +17276,7 @@ int skill_autospell(struct map_session_data *sd, uint16 skill_id)
 	else if(skill_id==MG_FROSTDIVER) maxlv=1;
 	else return 0;
 #endif
-	if(maxlv > lv)
-		maxlv = lv;
+	maxlv = min(lv, maxlv);
 
 	sc_start4(&sd->bl,&sd->bl,SC_AUTOSPELL,100,skill_lv,skill_id,maxlv,0,
 		skill_get_time(SA_AUTOSPELL,skill_lv));
